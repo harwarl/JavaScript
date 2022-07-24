0ce06596e130ae4874ba0c029df9c578dd7d0cb8
Oduwale Awwal <harwarl87@gmail.com>
2022-07-24 20:58:27 +0100

    using getelement by id javascript

---
+++ alert.js
@@ -0,0 +1,26 @@
+var popularNo = 2+2;
+console.log(popularNo);
+console.log(9%3);
+
+
+var num = 1;
+var newNum = num + 1;
+console.log(newNum);
+
+var writien = "this";
+console.log(writien + " is just a test");
+console.log("2"+"2");
+var message ="Thanks";
+var userName="Susan";
+var banger ="!";
+var customMessage = message + userName + banger;
+console.log(customMessage);
+
+var value =5;
+var val = 6;
+if (val === value){
+    console.log("true");
+}
+else{
+    console.log("False");
+}
